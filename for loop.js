554c92c190e2b0c75f4d5b4aeb35ef105f8c9f88
afomi <afomitlahun@gmail.com>
2026-07-10 22:02:29 +0300

    mam start

---
+++ for loop.js
@@ -0,0 +1,18 @@
+for (i=1;i<=10;i++){
+    if(i%2==0){
+    
+
+    console.log(i)
+    }
+}
+for(j=10;j>=1;j--){
+    console.log(j)
+}
+const fruit =["app" ,"bann","orang"];
+for(i=0;i<fruit.length;i++){
+    console.log(fruit[i])
+}
+const nu=[10,20,30,40];
+for (i=0;i<nu.length;i++){
+    console.log(nu[i])
+}
