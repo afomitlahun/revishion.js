52c4b25eb164d5ae02db0b0aa579748eb85b9b2a
afomi <afomitlahun@gmail.com>
2026-07-12 06:41:43 +0300

    loop chalang

---
+++ for loop chalang.js
@@ -0,0 +1,34 @@
+
+let sum=0;
+for(let i=1;i<=10;i++){
+    console.log(i) 
+sum+=i
+}
+
+    console.log(sum)
+
+    const nu =[10,20,30,70,90,60]
+    let max=nu[0]
+    for(let j=0;j<=nu.length;j++){
+        console.log(nu[j])
+        if(nu[j]>max)
+        max=nu[j]
+          
+    }
+console.log(max)
+
+const fruit =["app","bnnn","orr","mann"];
+for(let k=0;k<=fruit.length;k++){
+    console.log(fruit[k])
+}
+
+
+for(let t=1;t<=5;t++){
+    let star="";
+    for(let l=1;l<=t;l++ )
+    {
+
+    
+        star+="*";
+} console.log(star);
+}
